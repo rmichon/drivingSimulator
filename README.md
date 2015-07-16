3bbda63a55eb15cb4169cdd225b445105a90ad76
Romain Michon <rmnmichon@gmail.com>
2015-07-16 14:34:51 -0700

    First commit: adding udp2osc and updating the README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # drivingSimulator
+
+This repository contains the different softwares developed in the Frame of the Renault / CCRMA / CDR project conducted at Stanford Univeristy's Volkswagen Automotive Innovation Lab (VAIL).
+
+#Content
+
+- udp2osc: a very simple program that retrieves the UDP messages from the car simulator, format them into OSC messages and send them on a defined port.
---
+++ udp2osc/udp2osc.cpp
@@ -0,0 +1,113 @@
+/* 
+    Simple example of sending an OSC message using oscpack.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include "osc/OscOutboundPacketStream.h"
+#include "ip/UdpSocket.h"
+
+#define RECEIVE_PORT 5510
+#define SEND_ADDRESS "127.0.0.1"
+#define SEND_PORT 5511
+#define BUFFER_SIZE 2048
+
+//#define OUTPUT_BUFFER_SIZE 1024
+
+int main(int argc, char* argv[])
+{
+    (void) argc; // suppress unused parameter warnings
+    (void) argv; // suppress unused parameter warnings
+
+	struct sockaddr_in myaddr;	/* our address */
+	struct sockaddr_in remaddr;	/* remote address */
+	socklen_t addrlen = sizeof(remaddr);		/* length of addresses */
+	int recvlen;			/* # bytes received */
+	int fd;				/* our socket */
+	unsigned char buf[BUFFER_SIZE];	/* receive buffer */
+
+	/* create a UDP socket */
+
+	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("cannot create socket\n");
+		return 0;
+	}
+	
+    UdpTransmitSocket transmitSocket( IpEndpointName( SEND_ADDRESS, SEND_PORT ) );
+	
+	// The buffer out
+    char buffer[BUFFER_SIZE];
+    osc::OutboundPacketStream p( buffer, BUFFER_SIZE );
+    
+	/* bind the socket to any valid IP address and a specific port */
+
+	memset((char *)&myaddr, 0, sizeof(myaddr));
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	myaddr.sin_port = htons(RECEIVE_PORT);
+
+	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+		perror("bind failed");
+		return 0;
+	}
+	else{ 
+		printf("Listening to port %d\n", RECEIVE_PORT);
+	}
+	
+	/* now loop, receiving data and printing what we received */
+	for (;;) {
+		//printf("waiting on port %d\n", SERVICE_PORT);
+		recvlen = recvfrom(fd, buf, BUFFER_SIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+		//printf("received %d bytes\n", recvlen);
+		if (recvlen > 0) {
+			buf[recvlen] = 0;
+			//printf("Received: %s\n", inMessage.c_str());
+			
+			// retrieving the data from the udp message
+			// TODO: this will have to be updated in function of what the simulator will give
+			std::string inMessage( buf, buf + sizeof buf / sizeof buf[0] ); // converting the input buffer to a string
+			// retrieving the address
+			unsigned stringBegin = inMessage.find(";");
+			unsigned stringEnd = inMessage.find(":");
+			std::string oscAddress = inMessage.substr(stringBegin+1,stringEnd-stringBegin-1);
+			// retrieving the value
+			stringBegin = inMessage.find(":");
+			stringEnd = inMessage.find(";;");
+			std::string oscValueString = inMessage.substr(stringBegin+1,stringEnd-stringBegin-1);
+			float oscValue = stof(oscValueString);
+			
+			// formating and sending the OSC message
+		    p.Clear();
+		    p << osc::BeginMessage(oscAddress.c_str())
+		            << oscValue << osc::EndMessage;
+		    transmitSocket.Send( p.Data(), p.Size() );
+		}
+	}
+	/* never exits */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
